9e5473a645aa24eb939d9df811a38672023cc349
cpruvost <christophe.pruvost@oracle.com>
2022-09-20 15:37:23 +0200

    test with sqlcl version

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ async function run() {
     execSync('wget https://download.oracle.com/otn_software/java/sqldeveloper/sqlcl-latest.zip');
     execSync('unzip sqlcl-latest.zip');
     execSync('alias sql="./sqlcl/bin/sql"');
-    execSync('exit | sql ${user}@${user}/${password} @./show_version.sql');
+    execSync('exit | sql ${user}@${url}/${password} @./show_version.sql');
 
   } catch (error) {
     core.setFailed(error.message);
