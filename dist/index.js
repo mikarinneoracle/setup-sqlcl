d745aba7f5872bf57c1830373c94109cdb444cc9
cpruvost <christophe.pruvost@oracle.com>
2022-09-20 20:16:10 +0200

    first version

--- dist/index.js
+++ dist/index.js
@@ -1695,14 +1695,10 @@ async function run() {
       throw new Error('This action runs only on Linux currently');
     }
 
-    //const user = core.getInput('user');
-    //const password = core.getInput('password');
-    //const url = core.getInput('url');
-
     execSync('wget https://download.oracle.com/otn_software/java/sqldeveloper/sqlcl-latest.zip');
     execSync('unzip sqlcl-latest.zip');
-    //execSync('export PATH=$PATH:./sqlcl/bin/sql/bin');
-    //execSync(`sql -S ${user}@${url}/${password} @./show_version.sql`, {stdio: 'inherit'});
+    
+    //TO_DO : add an alias "sql"
 
   } catch (error) {
     core.setFailed(error.message);
