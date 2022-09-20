e491831cdf6d774856a6d83b69426e44d7f86b7e
cpruvost <christophe.pruvost@oracle.com>
2022-09-20 14:06:19 +0200

    first test

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const wait = require('./wait');
 // most @actions toolkit packages have async methods
 async function run() {
   try {
+    if(process.platform !== 'linux') {
+      throw new Error('This action runs only on Linux currently');
+    }
+
     const ms = core.getInput('milliseconds');
     core.info(`Waiting ${ms} milliseconds ...`);
 
@@ -13,6 +17,11 @@ async function run() {
     core.info((new Date()).toTimeString());
 
     core.setOutput('time', new Date().toTimeString());
+
+    execSync('wget https://download.oracle.com/otn_software/java/sqldeveloper/sqlcl-latest.zip');
+    execSync('unzip sqlcl-latest.zip');
+    execSync('alias sql="./sqlcl/bin/sql"');
+
   } catch (error) {
     core.setFailed(error.message);
   }
