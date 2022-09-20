d3f1e636554178078417396d04b29e1120bda5bc
cpruvost <christophe.pruvost@oracle.com>
2022-09-20 14:16:13 +0200

    first test

--- dist/index.js
+++ dist/index.js
@@ -1686,15 +1686,6 @@ async function run() {
       throw new Error('This action runs only on Linux currently');
     }
 
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-
     execSync('wget https://download.oracle.com/otn_software/java/sqldeveloper/sqlcl-latest.zip');
     execSync('unzip sqlcl-latest.zip');
     execSync('alias sql="./sqlcl/bin/sql"');
